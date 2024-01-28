5b9ed065ee2cafede18161a6ab066d5ac4f53ae8
Raj Kasaudhan <rajksd9@gmail.com>
2024-01-28 23:47:03 +0530

    @FOR @IF

--- projects/practice/src/app/contact-us/contact-us.component.ts
+++ projects/practice/src/app/contact-us/contact-us.component.ts
@@ -4,11 +4,18 @@ import { AppRegister } from '../components/register.component';
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [AppRegister], 
+  imports: [AppRegister],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css',
 })
 export class ContactUsComponent {
+  dataO = [
+    { key: 1, name: 'Raj' },
+    { key: 2, name: 'Harry' },
+    { key: 3, name: 'Sam' },
+    { key: 4, name: 'Krish' },
+  ];
+
   @Input() title!: string;
 
   @Output() data = new EventEmitter<string>();
